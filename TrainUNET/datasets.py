480705a8116ef653d0e963d24bff3cfc7fa0a7cf
noahvandal <noahvandal@outlook.com>
2023-05-25 13:55:49 -0500

    updated

--- TrainUNET/datasets.py
+++ TrainUNET/datasets.py
@@ -35,30 +35,38 @@ cellColor2Label = {label.color: label for label in cellLabels}
 
 
 class Dataset(Dataset):
+    '''
+    Dataloader for the images and masks dataset. 
+
+    Structure must be as follows:
+    rootDir
+        - Images
+        - Masks
+    with images and corresponding masks having the same name.
+    Will be loaded as pytorch tensor types. 
+
+
+    '''
     def __init__(self, rootDir, transform=None, eval=False):
         self.transform = transform
         self.maskList = []
         self.imgList = []
         self.eval = eval
 
-        # self.maskPath = os.path.join(os.getcwd(), rootDir + '/Images/')
-        # self.imgPath = os.path.join(os.getcwd(), rootDir + '/Masks/')
+
         self.maskPath = os.path.join(rootDir + '/Masks/')
         self.imgPath = os.path.join(rootDir + '/Images/')
 
-        # print('maskpath', self.maskPath, self.imgPath)
 
         ## since mask and image are the same name, we can just use one of them
         imgItems = os.listdir(self.imgPath)
-        # print(imgItems)
 
         maskItems = [rootDir + '/Masks/' + path for path in imgItems]
         imgItems = [rootDir + '/Images/' + path for path in imgItems]
 
         self.maskList.extend(maskItems)
         self.imgList.extend(imgItems)
-        # print(self.maskList)
-        # print(self.imgList)
+
 
     def __len__(self):
         length = len(self.imgList)
@@ -78,22 +86,16 @@ class Dataset(Dataset):
         
         img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
         img = transforms.ToTensor()(img)
-        # mask = transforms.ToTensor()(mask)
-        # mask = np.array(mask)
-        # print(mask.shape)
+
         mask = np.transpose(mask, [2, 0, 1])
         mask = rgbToOnehotBW(mask, cellColor2Label)
-        # print('mask shape', mask.shape)
 
-        # mask = mask[2, :, :]  # removing the alpha channel (not really needed)
         img = img.type(torch.FloatTensor)
         mask = torch.from_numpy(mask)
-        # mask = torch.from_numpy(mask)
         mask = mask.type(torch.LongTensor)
 
-        # print(type(img), type(mask))
-        # print(img.shape, mask.shape)
 
+        ## return corresponding name if is a validation image
         if self.eval:
             return img, mask, self.imgList[index]
         else:
@@ -101,9 +103,8 @@ class Dataset(Dataset):
 
         return img, mask
 
-# getting datset using dataload function and native functions from torch
 
-
+# getting datset using dataload function and native functions from torch; treated more like a generator
 def getDataset(rootDir, transform=None, batchSize=1, shuffle=True, pin_memory=True, eval=False):
     data = Dataset(rootDir=rootDir, transform=transform, eval=eval)
     dataLoaded = torch.utils.data.DataLoader(data, batch_size=batchSize,
@@ -111,17 +112,17 @@ def getDataset(rootDir, transform=None, batchSize=1, shuffle=True, pin_memory=Tr
 
     return dataLoaded
 
+
+
 # saving images when called upon
-
-
 def saveImages(tensor_pred, folder, image_name):
     tensor_pred = transforms.ToPILImage()(tensor_pred.byte())
     filename = f"{folder}\{image_name}.png"
     tensor_pred.save(filename)
 
+
+
 # function that is called when training model
-
-
 def trainFunction(data, model, optimizer, lossFunction, device):
     data = tqdm(data)
     for index, batch in enumerate(data):
@@ -161,39 +162,24 @@ wherever there is no cell, make it white with encoding position of 1.
 def rgbToOnehotBW(rgb, colorDict):
     rgb = np.array(rgb).astype('int32')
     rgb = np.transpose(rgb, [1, 2, 0])
-    # dense = np.zeros(rgb.shape[1:]) ## c, h ,w in --> hw outs
     dense = np.zeros(rgb.shape[:2] + (2,))
-    # print(dense.shape)
-    # dense = np.transpose(dense, [1, 2, 0])
-
-    # dense = np.expand_dims(dense, axis=0) ## new placeholder for color outpt
 
     black = np.array([1,0])
     white = np.array([0,1])
 
-    # blackcount = 0
-    # whitecount = 0
-
     for label, color in enumerate(colorDict.keys()):
-        ## whever cell region is, make black; else white. 
+        ## wherever cell region is, make black; else white. 
         ## since cell regions are first three colors from dictionary, will use those. 
         color = np.array(color)
-        # print(rgb.shape, color.shape, dense.shape)
         if label < (len(colorDict.keys())- 1):
-            # print(dense.shape, rgb.shape, color.shape, label)
             dense[np.all(rgb == color, axis=-1)] = black
-            # blackcount += np.sum(np.all(rgb == color, axis=-1))
         else:
             dense[np.all(rgb == color, axis=-1)] = white
-            # whitecount += np.sum(np.all(rgb == color, axis=-1))
-        
-    
-    
-    # print('blackcount', blackcount, 'whitecount', whitecount)
-    
+            
     return np.transpose(dense, [2, 0, 1])
 
-## instead of usign one-hot encoding, use sparse encoding
+
+## instead of usign one-hot encoding, use sparse encoding. Not implemented for our training
 def rgbToOnehotSparse(rgb, colorDict):
     rgb = np.array(rgb).astype('int32')
     dense = np.zeros(rgb.shape[:2])
@@ -206,11 +192,13 @@ def rgbToOnehotSparse(rgb, colorDict):
 
 
 def outputClassImages(onehot, color_dict):
+    '''
+    for dictionary of different class values, given a one-hot input (type of output of network and also truth mask), will output rgb image with correct label color scheme. 
+    '''
     onehot = np.array(onehot)
     onehot = np.argmax(onehot, axis=1)
     onehot = np.transpose(onehot, [1, 2, 0])
 
-    # numClasses = len(color_dict.keys())
     outputList = []  # temporarily store each image as it is created
     totalOutput = 255*np.ones(onehot.shape[0:2]+(3,))
 
@@ -225,8 +213,10 @@ def outputClassImages(onehot, color_dict):
     return outputList
 
 
-## instead of modifying class based output, will simply convert to bw whenever a non-background class is present. Used this when transferring from MC to binary
 def RGBtoBW(image, isGrayscale=False):
+    '''
+    This function was used when trying to perform inference using the MC model, but with binary output and classification. Not used in final result.
+    '''
     image = image.astype('uint8')
     bw = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY).astype('uint8')
     ret, bw = cv2.threshold(bw,254,255,cv2.THRESH_BINARY)  ## threshold to combine all colors in output channel
@@ -240,13 +230,12 @@ def RGBtoBW(image, isGrayscale=False):
 
 
 def onehotToIMG(img, bw=False):
+    '''
+    Another method to convert a one-hot encoded image to RGB.
+    '''
     img = np.array(img)
     output = np.zeros(img.shape[:2] + (3,))
     img = np.argmax(img, axis=-1, keepdims=True)
-
-    # print('img shape', img.shape)
-    # print('zeros', np.sum(np.all(img == 0, axis=-1)))
-    # print('ones', np.sum(np.all(img == 1, axis=-1)))
 
     output[np.all(img == 0, axis=-1)] = [0, 0, 0]
     output[np.all(img == 1, axis=-1)] = [255, 255, 255]
