017c277e98308525bb6ae3d3a9de3d6989aa3dea
noahvandal <noahvandal@outlook.com>
2023-05-25 13:55:37 -0500

    updated

--- TrainUNET/auxiliary.py
+++ TrainUNET/auxiliary.py
@@ -4,19 +4,17 @@ import torch
 
 
 def accuracy(output, target):
+    '''
+    just calculating accruacy, and other metrics wihin this function
+    '''
     output = output.cpu().detach().numpy()
     target = target.cpu().detach().numpy()
-    # print(output.shape, target.shape)
     output = np.argmax(output, axis=1)
     output = np.where(output > 0.5, 1, 0)
     target = np.argmax(target, axis=1)
-    # print(output.shape, target.shape)
-    # print(output, target)
 
     tp, tn, fp, fn = truthValues(output, target)
-    # print(tp, tn, fp, fn)
     precision, recall, f1, acc = precisionRecallF1Acc(tp, tn, fp, fn)
-    # print(precision, recall, f1, acc)
 
     xorImage = np.logical_xor(output, target)
 
@@ -24,6 +22,9 @@ def accuracy(output, target):
 
 
 def truthValues(matA, matB):
+    '''
+    find truth values from the two segmentation matrices
+    '''
     tp =  np.abs(np.sum(np.logical_and(matA == 1, matB == 1)))
     tn =  np.abs(np.sum(np.logical_and(matA == 0, matB == 0)))
     fp =  np.abs(np.sum(np.logical_and(matA == 1, matB == 0)))
@@ -33,6 +34,9 @@ def truthValues(matA, matB):
 
 
 def precisionRecallF1Acc(tp, tn, fp, fn):
+    '''
+    acquiring the precision, recall, f1, and accuracy values from the truth values
+    '''
     epsilon = 1 ## prevent from dividing by zero
     precision = tp / (tp + fp + epsilon)
     recall = tp / (tp + fn + epsilon)
@@ -42,30 +46,30 @@ def precisionRecallF1Acc(tp, tn, fp, fn):
 
 
 def IoU(output, target):
+    '''
+    Calculate the iou of the output and target segmentation masks
+    '''
     output = output.cpu().detach().numpy()
     target = target.cpu().detach().numpy()
     output = np.argmax(output, axis=1)
-    # output = np.where(output > 0.5, 1, 0)
     target = np.argmax(target, axis=1)
-    # print(output.shape, target.shape)
 
     #cell iou
     intersectCell = np.sum(np.logical_and(output == 0, target == 0))
     unionCell = np.sum(np.logical_or(output == 0, target == 0))
     iouCell = intersectCell / unionCell
-    # print(intersectCell, unionCell)
 
     ## background iou
     intersectBack = np.sum(np.logical_and(output == 1, target == 1))
     unionBack = np.sum(np.logical_or(output == 1, target == 1))
     iouBack = intersectBack / unionBack
-    # print(intersectBack, unionBack)
 
-    # print(intersectCell, unionCell, intersectBack, unionBack)
-    # print(iouCell, iouBack)
 
     return [iouCell, intersectCell, unionCell, iouBack]
 
 
 def calculateActualValue(value, batchsize, stepsize):
+    '''
+    use this for calculating the actual value of the loss function, if necessary 
+    '''
     return (value / stepsize) / batchsize
